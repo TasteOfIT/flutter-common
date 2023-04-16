0da617ea82246fedb3f6b738ae59e407c2b0ed51
Le Xing <xingled@gmail.com>
2023-04-16 12:37:42 +0800

    [Le] Move common widgets

---
+++ lib/widgets.dart
@@ -0,0 +1,6 @@
+export 'widgets/action_menu.dart';
+export 'widgets/app_bar.dart';
+export 'widgets/dividers.dart';
+export 'widgets/icon_text_menu.dart';
+export 'widgets/text_menu.dart';
+export 'widgets/view_dialogs.dart';
